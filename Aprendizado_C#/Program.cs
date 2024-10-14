3f13b31316bcdc1c55d92f5061e3f9fdad1902f9
Tiago Silva <pepinssantos@gmail.com>
2024-10-14 20:59:48 +0100

    doneTask function added, and (kinda of finalized project)

--- Aprendizado_C#/Program.cs
+++ Aprendizado_C#/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
 namespace taskManager{
     public static class taskManager
     { 
@@ -19,14 +22,19 @@ namespace taskManager{
 
         public static void removeTask(int taskID)
         {
-            tasks[taskID] = new task { id = -1, content = null, done = false };
+            if (tasks[taskID].content.Length <= 0)
+            {
+                System.Console.WriteLine("Task not found!\n");
+            }
+            tasks[taskID] = new task { id = 0, content = null, done = false };
+            System.Console.WriteLine("Task removed succesfully!\n");
         }
 
-        private static void listTask(int taskQuantity)
+        public static void listTask()
         {
-            for (int taskID = 0 ; taskID <= taskQuantity; ++taskID) 
-            { 
-                if (tasks[taskID].id != -1) 
+            for (int taskID = 1 ; taskID <= tasks.Length; ++taskID) 
+            {
+                if (tasks[taskID].content.Length >= 1) 
                 {
                     task actualTask = tasks[taskID];
                     System.Console.WriteLine($"{actualTask.id} {actualTask.content} {actualTask.done}");
@@ -48,27 +56,46 @@ namespace Aprendizado_C
         static void Main(string[] args) 
         {
             System.Console.WriteLine("This a task manager program in C#.\n");
-            taskIdTracker
+            int taskIdTracker = 1;
             while (true) 
             {
                 System.Console.WriteLine("Type 'add' to add a task, 'remove' to remove a task,");
-                System.Console.Write("and 'done' to mark a task as done: ");
+                System.Console.Write("'done' to mark a task as done and 'exit' to exit: ");
                 string option = System.Console.ReadLine();
+                option = option.ToLower();
+                System.Console.WriteLine(option);
+
+                if (option != "add" && option != "remove" && option != "done" && option != "exit") 
+                {
+                    continue;
+                }
+
                 switch (option) 
                 {
                     case "add": 
                         {
-                            System.Console.Write("Enter the task content");
-                            taskManager.taskManager.addTask()
+                            System.Console.Write("Enter the task content: ");
+                            taskManager.taskManager.addTask(taskIdTracker, System.Console.ReadLine());
                             break;
                         }
                     case "remove": 
                         {
+                            System.Console.WriteLine("Enter the task ID to remove: ");
+                            int taskID = Convert.ToInt32(System.Console.ReadLine());
+                            taskManager.taskManager.removeTask(taskID);
                             break;
                         }
                     case "done": 
                         {
+                            System.Console.WriteLine("Enter the task ID to mark as done: ");
+                            int taskID = Convert.ToInt32(System.Console.ReadLine());
+                            taskManager.taskManager.doneTask(taskID);
                             break;
+                        }
+                    case "exit":
+                        {
+                            System.Console.WriteLine("Exiting the program...");
+                            return;
                         }
                     default: 
                         { 
@@ -76,6 +103,7 @@ namespace Aprendizado_C
                             break;
                         }
                 }
+                taskManager.taskManager.listTask();
             }
         }
     }
